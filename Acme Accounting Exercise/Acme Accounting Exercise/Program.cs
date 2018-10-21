06ee1e492ff83579370a2192bce7c9c42714c40c
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-10-20 19:21:35 -0500

    Add files via upload

---
+++ Acme Accounting Exercise/Acme Accounting Exercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Acme_Accounting_Exercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Thread.Sleep(1000);
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
